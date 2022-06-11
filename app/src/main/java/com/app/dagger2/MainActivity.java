b76cf2742e434b93392325d4b93eb5a53f84b74f
abhijit_p_patel <patelabhijit49@gmail.com>
2022-06-12 01:06:29 +0530

    this is second commit

--- app/src/main/java/com/app/dagger2/MainActivity.java
+++ app/src/main/java/com/app/dagger2/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         CarComponent carComponent = DaggerCarComponent.create();
         //car = carComponent.getCar();
 
